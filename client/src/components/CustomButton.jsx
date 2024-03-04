1b45956aada2d5c90748e8be43f1b6695c2c932b
Chandan-Bisht <bishtchandan023@gmail.com>
2024-03-04 21:57:10 +0530

    Added login page

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,11 @@
+
+const CustomButton = ({ title, containerStyles, iconRight, type, onClick}) => {
+  return (
+    <button type={type || "button"} onClick={onClick} className={`inline-flex items-center text-base ${containerStyles}`}>
+        {title}
+        {iconRight && <div className="ml-2">{iconRight}</div>}
+    </button>
+  )
+}
+
+export default CustomButton;
