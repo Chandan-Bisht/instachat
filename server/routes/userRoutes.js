92788e72664e83be7fba459c4aa59a6abb3441b3
Chandan-Bisht <bishtchandan023@gmail.com>
2024-03-21 21:39:35 +0530

    Added reset password functionality in the backend

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
-import { verifyEmail } from "../controllers/userController.js";
+import { changePassword, getUser, requestPasswordReset, resetPassword, updateUser, verifyEmail } from "../controllers/userController.js";
+import userAuth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
@@ -8,7 +9,20 @@ const _dirname = path.resolve(path.dirname(""));
 
 router.get("/verify/:userId/:token", verifyEmail);
 
+// RESET PASSWORD
+router.post("/request-passwordreset", requestPasswordReset);
+router.get("/reset-password/:userId/:token", resetPassword);
+router.post("/reset-password", changePassword);
+
+// user routes
+router.post("/get-user/:id?", userAuth, getUser);
+router.put("/update-user", userAuth, updateUser);
+
 router.get("/verified", (req, res) => {
+    res.sendFile(path.join(_dirname, "./views/build", "index.html"));
+})
+
+router.get("/resetpassword", (req, res) => {
     res.sendFile(path.join(_dirname, "./views/build", "index.html"));
 })
 
